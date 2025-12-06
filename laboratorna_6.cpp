1c2f668111636ab2bba5ee787cd24b450b349f99
Bogia_Leg <bogialeg@gmail.com>
2025-12-06 17:56:50 +0200

    повністю готовий файл

--- laboratorna_6.cpp
+++ laboratorna_6.cpp
@@ -3,12 +3,20 @@ using namespace std;
 
 int main()
 {
-    int A[12];
+    int A[12], neparni, C[12];
     // введення елементів мисиву
     for (int i = 0; i < 12; i++)
     {
         cin >> A[i];
     }
-      
+    // перевіряємо елементи чи вони парні
+    for (int i = 0; i < 12; i++)
+    {
+        if (neparni >= 2)
+        {
+            C[i]=A[i]/2;
+        }
+        else 
+    } 
     return 0;
 }
