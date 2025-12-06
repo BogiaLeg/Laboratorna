f2108ad6b78d36bb8dc0fdb67ba8ec3cf2b572c1
BogiaLeg <bogialeg@gmail.com>
2025-12-06 18:26:47 +0200

    Revert "повністю готовий файл"

--- laboratorna_6.cpp
+++ laboratorna_6.cpp
@@ -3,20 +3,12 @@ using namespace std;
 
 int main()
 {
-    int A[12], neparni, C[12];
+    int A[12];
     // введення елементів мисиву
     for (int i = 0; i < 12; i++)
     {
         cin >> A[i];
     }
-    // перевіряємо елементи чи вони парні
-    for (int i = 0; i < 12; i++)
-    {
-        if (neparni >= 2)
-        {
-            C[i]=A[i]/2;
-        }
-        else 
-    } 
+      
     return 0;
 }
